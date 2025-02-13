b294444a45aa2c3dfdff3ecc5aa3be7ec4346461
Adarsh Mohan S <125942633+adarshmohans@users.noreply.github.com>
2025-02-14 02:57:49 +0530

    Displaying Categories on the page using a Table

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -5,5 +5,34 @@
     <div class="card-body">
         <h1>Categories</h1>
     </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($category as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>
+                        <img src="{{ asset('assets/uploads/category/'.$item->image) }}" width="200" alt="image here">
+                    </td>
+                    <td>
+                        <button class="btn btn-primary">Edit</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
