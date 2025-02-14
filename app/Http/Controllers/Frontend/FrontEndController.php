88b015afdaec63734090e1cad1efdd0a7e26da7a
Adarsh Mohan S <125942633+adarshmohans@users.noreply.github.com>
2025-02-15 03:34:32 +0530

    Integrating owl carousel (Slider for displaying products)

--- app/Http/Controllers/Frontend/FrontEndController.php
+++ app/Http/Controllers/Frontend/FrontEndController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
 {
     public function index(){
-        return view('frontend.index');
+        $featured_products= Product::where('trending', '1')->get();
+        return view('frontend.index', compact('featured_products'));
     }
 }
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -12,6 +12,11 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Owl carousel Styles -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+
 </head>
 <body>
     @include('layouts.inc.frontnav')
@@ -20,13 +25,15 @@
     </div>
 
 
-
-
-
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+    <!-- Owl carousel Scripts -->
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
 
     @if(session('status'))
     <script>
