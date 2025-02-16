166c1922514d40faf3f6858f420a6b25c664fbe7
Adarsh Mohan S <125942633+adarshmohans@users.noreply.github.com>
2025-02-17 03:42:10 +0530

    Fontawesone (Love and Cart button)

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', $products->name)
 
+
 @section('content')
 <div class="py-3 mb-4 shadow-sm bg-warning border-top">
     <div class="container">
@@ -46,8 +47,8 @@
                         </div>
                         <div class="col-md-10">
                             <br>
-                            <button type="button" class="btn btn-success me-3 float-start">Add to Wishlist</button>
-                            <button type="button" class="btn btn-primary me-3 float-start">Add to Cart</button>
+                            <button type="button" class="btn btn-success me-3 float-start">Add to Wishlist<i class="fa fa-heart"></i></button>
+                            <button type="button" class="btn btn-primary me-3 float-start">Add to Cart<i class="fa fa-shopping-cart"></i></button>
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -17,6 +17,9 @@
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
 
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <style>
         a {
             text-decoration: none !important;
