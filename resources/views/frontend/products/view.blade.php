9ee2375dbd1af844c0d250d7d511777a573c8bca
Adarsh Mohan S <125942633+adarshmohans@users.noreply.github.com>
2025-02-23 21:28:15 +0530

    Checking stock availability

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -48,8 +48,10 @@
                         </div>
                         <div class="col-md-10">
                             <br>
-                            <button type="button" class="btn btn-success me-3  float-start">Add to Wishlist<i class="fa fa-heart"></i></button>
+                            @if($products->qty > 0)
                             <button type="button" class="btn btn-primary me-3 addToCartBtn float-start">Add to Cart<i class="fa fa-shopping-cart"></i></button>
+                            @endif
+                            <button type="button" class="btn btn-success me-3  float-start">Add to Wishlist<i class="fa fa-heart"></i></button>
                         </div>
                     </div>
                 </div>
